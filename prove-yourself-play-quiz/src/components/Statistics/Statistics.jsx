0aed3e4047e39913cbcb788bc6d1f911b7d10f66
HasanBakar <abubakar.cse@gmail.com>
2022-10-12 02:46:34 +0600

    Statistics page responsive format added

--- prove-yourself-play-quiz/src/components/Statistics/Statistics.jsx
+++ prove-yourself-play-quiz/src/components/Statistics/Statistics.jsx
@@ -9,7 +9,7 @@ const Statistics = () => {
     // console.log(data);
     return (
            <div className="graph-container">
-                    <LineChart className="shadow-lg pl-1 pr-2 shadow-red-500 rounded-xl py-3"  width={700}
+                    <LineChart className="hw-reset-container shadow-lg pl-1 pr-2 shadow-red-500 rounded-xl py-3 ml-10"  width={600}
                             height={400}
                             data={data}
                                margin={{
@@ -27,7 +27,7 @@ const Statistics = () => {
                             <Line type="monotone" dataKey="total" stroke="#82ca9d"></Line>
                             <Line type="monotone" dataKey="id" stroke="#8884d8" activeDot={{ r: 8 }} />
                     </LineChart>   
-                    <div className="shadow-lg px-12 shadow-red-500 rounded-xl pt-3" >
+                    <div className="shadow-lg px-10 shadow-red-500 rounded-xl py-3" >
                         <h1 className="text-4xl font-bold text-fuchsia-600" >What does mean by line chart?</h1>
                         <p className="text-xl font-medium" >A line graph is commonly used to display change over time as a series of data points connected by straight line segments on two axes. The line graph therefore helps to determine the relationship between two sets of values, with one data set always being dependent on the other set.</p>
                         <p className="text-xl font-medium" >Line graphs are useful in that they show data variables and trends very clearly and can help to make predictions about the results of data not yet recorded. They can also be used to display several dependent variables against one independent variable. When comparing data sets, line graphs are only useful if the axes follow the same scales. Some experts recommend no more than 4 lines on a single graph; any more than that and it becomes difficult to interpret.</p>
